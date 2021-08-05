424b9d596371819d6ea2451313324e4032e44bf8
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-08-05 10:34:40 +0300

    Update HookController.cs: Hook Moving Logic

--- Assets/Scripts/HookController.cs
+++ Assets/Scripts/HookController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 using DG.Tweening;
 
 [SelectionBase]
@@ -6,15 +7,18 @@ public class HookController : MonoBehaviour
 {
     [Header("Debug")]
     [SerializeField] GameObject hookedObject;
-    [SerializeField] bool canMove;
+    [SerializeField] Button hookButton;
+    [SerializeField] bool isHookMoving;
     [SerializeField] int length;
     [SerializeField] int strength;
 
     [Header("Normal")]
     [Range(1f, 10f)]
     [SerializeField] float fishingDownwardsTime;
+
     [Range(1f, 10f)]
     [SerializeField] float fishingUpwardsTime;
+
     [Range(0f, 1f)]
     [SerializeField] float fishingUpwardsBurstTime;
 
@@ -38,29 +42,30 @@ public class HookController : MonoBehaviour
 
     private void Start()
     {
-        canMove = true;
+        isHookMoving = false;
         collider.enabled = false;
         startPos = transform.position;
     }
 
     private void Update()
     {
-        Move();
+        if (isHookMoving)
+        {
+            Move();
+        }
     }
 
     private void Move()
     {
-        if (canMove && Input.GetMouseButton(0))
-        {
-            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
-        }
+        //Move Hook Along X-Axis
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
     }
 
     public void StartFishing()
     {
-        length = -50;
-        strength = 3;
+        isHookMoving = true;
+        hookButton.gameObject.SetActive(false);
         fishCount = 0;
         camTweener = cam.transform.DOMoveY(length, fishingDownwardsTime).OnUpdate(delegate
         {
@@ -80,12 +85,10 @@ public class HookController : MonoBehaviour
              });
         });
         collider.enabled = false;
-        canMove = true;
     }
 
     private void StopFishing()
     {
-        canMove = false;
         camTweener.Kill(false);
         camTweener = cam.transform.DOMoveY(0, fishingUpwardsBurstTime).OnUpdate(delegate
              {
@@ -97,6 +100,8 @@ public class HookController : MonoBehaviour
              }).OnComplete(delegate
              {
                  collider.enabled = true;
+                 hookButton.gameObject.SetActive(true);
+                 isHookMoving = false;
              });
     }
 }
